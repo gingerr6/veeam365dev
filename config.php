3632c49e7e0b783cadb4c4d83d13e7153a500de9
gingerr6 <74706187+gingerr6@users.noreply.github.com>
2021-01-07 13:43:45 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'bcdrdemovccsp01.uksouth.cloudapp.azure.com'; /* Veeam Backup for Microsoft Office 365 server (hostname or IP) */
-$port = '4443'; /* RESTful API service port (default: 4443) */
+$port = '4444'; /* RESTful API service port (default: 4443) */
 $title = 'Veeam Backup for Microsoft Office 365 DEV'; /* Custom title for the portal to be displayed in the browser title */
 $version = 'v5'; /* RESTful API version (default: v4) */
 $administrator = 'yes'; /* Allow Windows administrator accounts to be used as a login (yes or no) */
