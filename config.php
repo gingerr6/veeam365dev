fb6c064d0ee041d50abefb2b37412c9e568dea5e
gingerr6 <74706187+gingerr6@users.noreply.github.com>
2021-01-08 13:09:14 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'bcdrdemovccsp01.uksouth.cloudapp.azure.co'; /* Veeam Backup for Microsoft Office 365 server (hostname or IP) */
+$host = 'bcdrdemovccsp01.uksouth.cloudapp.azure.com'; /* Veeam Backup for Microsoft Office 365 server (hostname or IP) */
 $port = '4443'; /* RESTful API service port (default: 4443) */
 $title = 'Veeam Backup for Microsoft Office 365 DEV'; /* Custom title for the portal to be displayed in the browser title */
 $version = 'v5'; /* RESTful API version (default: v4) */
